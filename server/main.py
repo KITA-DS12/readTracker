87a4031d071931f623018619be4a77a7bb3338dc
KITA-DS12 <kita.ds12@gmail.com>
2023-03-15 03:58:47 +0900

    add get/post

--- server/main.py
+++ server/main.py
@@ -1,29 +1,38 @@
 from fastapi import FastAPI
+from pydantic import BaseModel
 import uvicorn
 
-# FastAPIのインスタンスを作成
 app = FastAPI()
 
 
-# ルートパス("/")へアクセスした場合
 @app.get("/")
 async def root():
-    # JSON形式で"message": "Hello World"というメッセージを返す
     return {"message": "Hello World"}
 
-# このpyファイルが実行された場合
+
+@app.get("/test1")
+async def test1(name: str = ""):
+    return {"message": name}
+
+
+@app.get("/test2/{name}")
+async def test2(name: str = ""):
+    return {"message": name}
+
+
+class User(BaseModel):
+    name: str
+
+
+@app.post("/test3")
+async def test3(user: User):
+    return {"message": user.name}
+
 if __name__ == "__main__":
-    # WSGI(Web Server Gateway Interface)であるuvicornで
-    # サーバーを起動する
     uvicorn.run(
-        # main.pyファイルのappオブジェクトを使用する
         app="main:app",
-        # ホストに0.0.0.0を指定
         host="0.0.0.0",
-        # ファイル変更時に自動でリロードをする
         reload=True,
-        # ポート番号8888を指定
         port=8888,
-        # デバッグログを出力
         log_level="debug"
     )
