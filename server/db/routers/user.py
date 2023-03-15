1750500b65cbcf5dfe5c8848213ac527988af801
KITA-DS12 <kita.ds12@gmail.com>
2023-03-15 18:38:31 +0900

    fix server-signup && add server-signin

--- server/db/routers/user.py
+++ server/db/routers/user.py
@@ -8,34 +8,6 @@ from db.database import get_db
 router = APIRouter()
 
 
-@router.post("/user", response_model=schemas.User, tags=['user'])
-def create_user(user: schemas.UserCreate, db: Session = Depends(get_db)):
-    """
-    Userを作成する
-
-    Parameters
-    ----------
-    user : schemas.UserCreate
-        ユーザ名とパスワードを含んだデータ
-    db : Session
-        データベースとの接続を行うための情報
-
-    Returns
-    -------
-    new_user : models.User
-        Userモデル
-    """
-
-    db_user = crud.read_user_by_name(db=db, user_name=user.name)
-    # 既にユーザ名が存在する場合は400エラーを返す
-    if db_user:
-        raise HTTPException(
-            status_code=400, detail="Username already registered")
-
-    new_user = crud.create_user(db=db, user=user)
-    return new_user
-
-
 @router.get("/users", response_model=list[schemas.User], tags=['user'])
 def read_users(offset: int = 0,
                limit: int = 100,
@@ -82,3 +54,58 @@ def read_user_by_id(user_id: int, db: Session = Depends(get_db)):
 
     user = crud.read_user_by_id(db=db, user_id=user_id)
     return user
+
+
+@router.post("/user/signup", response_model=schemas.User, tags=['user'])
+def signup(user: schemas.UserCreate, db: Session = Depends(get_db)):
+    """
+    Userを作成する
+
+    Parameters
+    ----------
+    user : schemas.UserCreate
+        ユーザ名とパスワードを含んだデータ
+    db : Session
+        データベースとの接続を行うための情報
+
+    Returns
+    -------
+    new_user : models.User
+        Userモデル
+    """
+
+    db_user = crud.read_user_by_name(db=db, user_name=user.name)
+    # 既にユーザ名が存在する場合は400エラーを返す
+    if db_user:
+        raise HTTPException(
+            status_code=400, detail="Username already registered")
+
+    new_user = crud.create_user(db=db, user=user)
+    return new_user
+
+
+@router.post("/user/signin", response_model=schemas.User, tags=['user'])
+def signin(user: schemas.UserCreate, db: Session = Depends(get_db)):
+    """
+    アカウント認証を行う
+
+    Parameters
+    ----------
+    user : schemas.UserCreate
+        ユーザ名とパスワードを含んだデータ
+    db : Session
+        データベースとの接続を行うための情報
+
+    Returns
+    -------
+    user : models.User
+        Userモデル
+    """
+
+    db_user = crud.read_user_by_name(db=db, user_name=user.name)
+    # パスワードが一致しない場合は401エラーを返す
+    if user.password != db_user.password:
+        raise HTTPException(
+            status_code=401, detail="Password is incorrect")
+
+    return db_user
