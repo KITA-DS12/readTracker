fd253826d38556833949fa6b2d3b9194d3e5bd63
KITA-DS12 <kita.ds12@gmail.com>
2023-03-16 23:41:56 +0900

    add server-origin

--- server/main.py
+++ server/main.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 import uvicorn
 
 from db.database import engine
@@ -8,6 +9,14 @@ from db import routers
 models.Base.metadata.create_all(bind=engine)
 
 app = FastAPI()
+origins = ["http://localhost:5173"]
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 app.include_router(routers.router)
 
 
